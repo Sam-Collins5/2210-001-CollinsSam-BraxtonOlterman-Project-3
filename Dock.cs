49c5beb44c53e5d3d8131381fc112d2fd5195092
Sam-Collins5 <113549560+Sam-Collins5@users.noreply.github.com>
2023-10-28 17:26:59 -0400

    Add files via upload

---
+++ Dock.cs
@@ -0,0 +1,45 @@
+﻿/**       
+ *--------------------------------------------------------------------
+ *          Author:         Sam Collins (collinss5@etsu.edu)				
+ *          Course-Section: CSCI-2210-001
+ *          Assignment:     Project 3
+ *          Description:    A class that represents a truck loading dock.	
+ * -------------------------------------------------------------------
+ */
+
+namespace _2210_001_CollinsSam_BraxtonOlterman_Project_3
+{
+    class Dock
+    {
+        public string Id { get; set; }
+        public Queue<Truck> Lines { get; set; }
+        public double TotalSales { get; set; }
+        public int TotalCrates { get; set; }
+        public int TotalTrucks { get; set; }
+        public int TimeInUse { get; set; }
+        public int TimeNotInUse { get; set; }
+
+        public Dock(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                throw new InvalidDataException();
+            Id = id;
+            Lines = new Queue<Truck>();
+            TotalSales = 0.0;
+            TotalCrates = 0;
+            TotalTrucks = 0;
+            TimeInUse = 0;
+            TimeNotInUse = 0;
+        }
+
+        void JoinLine(Truck truck)
+        {
+            Lines.Enqueue(truck);
+        }
+
+        Truck SendOff()
+        {
+            return Lines.Dequeue();
+        }
+    }
+}
---
+++ Warehouse.cs
@@ -0,0 +1,28 @@
+﻿/**       
+ *--------------------------------------------------------------------
+ *          Author:         Sam Collins (collinss5@etsu.edu)				
+ *          Course-Section: CSCI-2210-001
+ *          Assignment:     Project 3
+ *          Description:    A class that represents a warehouse.
+ * -------------------------------------------------------------------
+ */
+
+namespace _2210_001_CollinsSam_BraxtonOlterman_Project_3
+{
+    class Warehouse
+    {
+        public List<Dock> Docks;
+        public Queue<Truck> Entrance;
+
+        public Warehouse()
+        {
+            Docks = new List<Dock>();
+            Entrance = new Queue<Truck>();
+        }
+
+        void Run()
+        {
+
+        }
+    }
+}
