2e7910a26b6a85516801bfe2344a099441bca35d
Braxton2719 <113900431+Braxton2719@users.noreply.github.com>
2023-10-28 16:27:44 -0400

    Add files via upload
    Crate class

---
+++ Crate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2210_001_CollinsSam_BraxtonOlterman_Project_3
+{
+    class Crate
+    {
+        public string ID { get; set; }
+        public double Price { get; set; }
+
+        public Crate(string id)
+        {
+            ID = id;
+            Random randy = new Random();
+            Price = Math.Round(randy.NextDouble() * (500 - 50) + 50, 2);
+        }
+
+    }
+}
