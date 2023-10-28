1bc1a02ea9a969e6c4de03d6b00cdc9d8c9e57a7
Sam-Collins5 <113549560+Sam-Collins5@users.noreply.github.com>
2023-10-28 15:43:08 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/**       
+ *--------------------------------------------------------------------
+ *          Course-Section: CSCI-2210-001
+ *          Assignment:     Project 3
+ * -------------------------------------------------------------------
+ */
+
+namespace _2210_001_CollinsSam_BraxtonOlterman_Project_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
