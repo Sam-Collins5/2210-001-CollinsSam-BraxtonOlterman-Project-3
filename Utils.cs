cfa00050baa5cde1a400d27b18dbddf19a2296ae
Sam-Collins5 <113549560+Sam-Collins5@users.noreply.github.com>
2023-11-16 12:33:26 -0500

    Add files via upload

--- Utils.cs
+++ Utils.cs
@@ -3,10 +3,24 @@ namespace _2210_001_CollinsSam_BraxtonOlterman_Project_3
 {
     class Utils
     {
-        // TODO (Braxton): Make this function
-        public void LogCratesToCSV(Truck truck, string filepath)
+        /// <summary>
+        /// Writes to the file at the given file path and writes the given content into it.
+        /// </summary>
+        /// <param name="filePath">The file path of the file to write to</param>
+        /// <param name="content">The content to write to the file</param>
+        public static void WriteToFile(string filePath, string content)
         {
-
+            try
+            {
+                using (StreamWriter wtr = new StreamWriter(filePath))
+                {
+                    wtr.Write(content);
+                }
+            }
+            catch
+            {
+                Console.WriteLine($"The file at path \"{filePath}\" could not be written to!");
+            }
         }
     }
 }
